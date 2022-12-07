f15cccffd8fd1aede115554fe4086f73a5867e41
mogiven <2359353909@qq.com>
2022-12-08 02:30:40 +0800

    apply

---
+++ src/components/home/Apply.vue
@@ -0,0 +1,118 @@
+<template>
+    <div>  
+        <el-form :model="Form" label-width="80px">
+            <el-form-item label="投递用户">
+              <span>{{Form.userId}}</span>
+            </el-form-item>
+
+           
+            <el-form-item label="年龄/岁">
+                <el-col :span="5">
+                    <el-input v-model="Form.age" />
+                </el-col>
+                
+                <el-col :span="2">
+                    <span style="margin-left: 20px;">性别</span>
+                  </el-col>
+
+                <el-col :span="3">
+                    <el-select v-model="Form.sex" placeholder="性别">
+                        <el-option label="男" value="男" />
+                        <el-option label="女" value="女" />
+                      </el-select>
+                </el-col>
+            </el-form-item>
+            
+
+            <el-col :span="10">
+                <el-form-item label="邮箱">
+                    <el-input v-model="Form.email" />
+                </el-form-item>
+            </el-col>
+
+            
+            <el-form-item label="学历">
+                <el-col :span="4">
+                    <el-select v-model="Form.edu" placeholder="学历" >
+                        <el-option label="博士" value="博士" />
+                        <el-option label="硕士" value="硕士" />
+                        <el-option label="本科" value="本科" />
+                        <el-option label="大专" value="大专" />
+                        <el-option label="其他" value="其他" />
+                    </el-select>
+                </el-col>
+            </el-form-item>
+            
+            <el-form-item label="自我介绍">
+                <el-input v-model="Form.description" type="textarea" />
+            </el-form-item>
+
+            <el-form-item >
+                <el-button type="primary" :icon="Check" @click="submit()" >确认投递</el-button>
+              </el-form-item>
+        </el-form>
+        
+        
+    </div>
+</template>
+
+<script lang="ts">
+import {useStore } from "vuex";
+import { Check} from '@element-plus/icons-vue'
+import axios from "axios";
+
+export default{
+    props:{
+        //从父组件获取的参数
+        profileId:{
+            type:Object,
+            required:true,
+        },
+    },
+    data(){
+
+        return {
+            Check,
+            Form:{
+                userId: useStore().state.userId,
+                age:"",
+                sex:"",
+                email:"",
+                edu:"",
+                description:"",
+            }
+        }
+    },
+    methods:{
+        async submit(){
+            let that = this;
+            alert(that.profileId);
+            //post表单进个人简历,但是哄，简历那边还只是一个对象，需要进行修改，现就这样
+            axios({
+                method: "post",
+                url: "http://localhost:9090/",
+                params:{
+                    //解析称url的地方
+                    
+                },
+                data:{
+                    //放在body的地方
+
+                },
+            }).then(function (response) {
+                if(response.data.code == "200"){
+                   
+
+                }else{
+                    alert("error");
+                }
+            });
+
+        }
+    },
+    mounted(){
+        // alert(this.profileId);
+    }
+}
+
+</script>
--- src/components/home/RecruitArea.vue
+++ src/components/home/RecruitArea.vue
@@ -10,7 +10,9 @@
               <template #header>
                 <div class="card-header">
                   <span style="font-size:larger">{{recruitObj.title}}</span>
-                  <el-button class="button" type="success">Apply</el-button>
+                  <el-button class="button" type="success" @click="apply(recruitObj.id)"> Apply</el-button>
+                  <!-- @click="apply(recruitObj.id)" -->
+                
                 </div>
               </template>
               <div class="salary">
@@ -22,31 +24,42 @@
               
               <div class="description">
                 {{recruitObj.description}}
-              </div>
-                  
+              </div>              
+              
           </el-card> 
           </div>
       </div>
+
+      <el-dialog v-model="dialogApplyVisible" title="请填写简历信息">
+        <Apply :profileId="select_id"></Apply>
+      </el-dialog>
+      
   </template>
   
 <script>
 import { ref } from 'vue';
 import axios from "axios";
+import Apply from "@/components/home/Apply.vue";
 
 export default{
+  components:{
+    Apply,
+  },
   data(){
    
     return {
+      select_id:"",
+      dialogApplyVisible: false,
       recruitObjs :[
         {
-          index:"1",
+          id:"1",
           title:"蔚蓝求职",
           position:"算法工程师",
           salary:"1.5w-2w",
           description:"快来家人们",
         },
         {
-          index:"2",
+          id:"2",
           title:"性感腾讯在线招人",
           position:"游戏架构师",
           salary:"5w-8w",
@@ -57,6 +70,12 @@ export default{
     }
   },
   methods:{
+
+    apply(id){
+      this.dialogApplyVisible = true;
+      this.select_id = id;
+      // alert(this.select_id);
+    },
 
     handleChange(val){
       console.log(val)
@@ -73,6 +92,7 @@ export default{
         response.data.data.forEach(element => {
           that.recruitObjs.push(
             {
+              id:element.id,
               title:element.title,
               position:element.position,
               salary:element.salary,
